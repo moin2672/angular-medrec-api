1e3e9038cb2d91633f40750d10126d471108f440
moin2672 <moin2672@gmail.com>
2021-10-13 17:37:28 +0530

    3. testing middleware with sample data for GET method

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 //middleware
-app.use((req, res, next)=>{
-    console.log('First Middleware')
-    next()
+// app.use((req, res, next)=>{
+//     console.log('First Middleware')
+//     next()
+// });
+
+app.use('/api/subjects',(req, res, next)=>{
+    const subjects=[{subjectAadhar:"title", subjectName: "content"},{subjectAadhar:"title", subjectName: "content"},{title:"title", content: "content"}];
+    res.status(200).json({message:"Subjects fetched successfully", subjects:subjects});
 });
 
 app.use((req, res, next)=>{
--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const onListening = () => {
     debug("Listening on "+bind);
 };
 
-const port = normalizePort(process.env.PORT || "3002");
+const port = normalizePort(process.env.PORT || "3003");
 console.log(port);
 
 app.set('port', port);
