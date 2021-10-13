12e7a18c40a272f360d212e646f7e4209265f502
moin2672 <moin2672@gmail.com>
2021-10-13 20:06:18 +0530

    7. mongodb connection string & app.use changed to app.get & app.post & app.delete method updated

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const mogoose = require("mongoose");
 
 const Subject = require('./models/subject');
 
 const app = express();
+
+mogoose.connect("mongodb+srv://shafmoin:oOdRnO3aBwTXsNCZ@cluster0.qmsyy.mongodb.net/medrec", { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=>{console.log("Connected to Database")})
+        .catch(()=>{console.log("Db connection failed!")});
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -22,12 +27,37 @@ app.post('/api/subjects',(req, res, next) =>{
         subjectAadhar: req.body.subjectAadhar,
         subjectName: req.body.subjectName
     });
-    res.status(201).json({message:"Subject added successfully!"});
+    subject.save().then(createdSubject=>{
+        res.status(201).json({
+            message:"Subject added successfully!",
+            postId: createdSubject._id
+        });
+    })
+    .catch(()=>{
+        console.log("Subject NOT saved")
+    });
 })
 
-app.use('/api/subjects',(req, res, next)=>{
-    const subjects=[{id:"asdfasdf3w423",subjectAadhar:"title", subjectName: "content"},{id:"qwerqew798798wer",subjectAadhar:"title", subjectName: "content"},{id:"adssf7687hkh",subjectAadhar:"title", subjectName: "content"}];
-    res.status(200).json({message:"Subjects fetched successfully", subjects:subjects});
+app.get('/api/subjects',(req, res, next)=>{
+    Subject.find()
+        .then(documents=>{
+            res.status(200).json({
+                message:"Subject fetched successfully", 
+                subjects:documents
+            });
+        })
+        .catch(()=>{console.log("Unable to get documents")});
+});
+
+app.delete('/api/subjects/:id',(req, res, next)=>{
+    Subject.deleteOne({_id:req.params.id})
+    .then(result=>{
+        console.log(result);
+        res.status(200).json({message:"Subject Deleted!"})
+    })
+    .catch(()=>{
+        console.log("Subject is not deleted")
+    })
 });
 
 app.use((req, res, next)=>{
