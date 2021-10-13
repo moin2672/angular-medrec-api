1228ade7d4216634ed1a1eb7734527d5e59ab467
moin2672 <moin2672@gmail.com>
2021-10-13 18:02:33 +0530

    4. ADDING CORS method

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 
 //middleware
-// app.use((req, res, next)=>{
-//     console.log('First Middleware')
-//     next()
-// });
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next()
+});
 
 app.use('/api/subjects',(req, res, next)=>{
-    const subjects=[{subjectAadhar:"title", subjectName: "content"},{subjectAadhar:"title", subjectName: "content"},{title:"title", content: "content"}];
+    const subjects=[{id:"asdfasdf3w423",subjectAadhar:"title", subjectName: "content"},{id:"qwerqew798798wer",subjectAadhar:"title", subjectName: "content"},{id:"adssf7687hkh",subjectAadhar:"title", subjectName: "content"}];
     res.status(200).json({message:"Subjects fetched successfully", subjects:subjects});
 });
 
