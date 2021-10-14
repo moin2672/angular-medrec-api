41a72d08a0b4e94c63214f94645e68ebfb8ada8b
moin2672 <moin2672@gmail.com>
2021-10-14 15:12:33 +0530

    9. restructuring the model and routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mogoose = require("mongoose");
 
-const Subject = require('./models/subject');
+const subjectRoutes = require("./routes/subjects");
 
 const app = express();
 
@@ -20,77 +20,9 @@ app.use((req, res, next)=>{
     next()
 });
 
-app.post('/api/subjects',(req, res, next) =>{
-    // const subject=req.body;
-    console.log(req.body);
-    const subject=new Subject({
-        subjectAadhar: req.body.subjectAadhar,
-        subjectName: req.body.subjectName
-    });
-    subject.save().then(createdSubject=>{
-        console.log("subject added success")
-        console.log(createdSubject._id)
-        res.status(201).json({
-            message:"Subject added successfully!",
-            subjectId: createdSubject._id
-        });
-    })
-    .catch(()=>{
-        console.log("Subject NOT saved")
-    });
-})
+app.use("/api/subjects",subjectRoutes);
 
-app.get('/api/subjects',(req, res, next)=>{
-    Subject.find()
-        .then(documents=>{
-            res.status(200).json({
-                message:"Subject fetched successfully", 
-                subjects:documents
-            });
-        })
-        .catch(()=>{console.log("Unable to get documents")});
-});
 
-app.delete('/api/subjects/:id',(req, res, next)=>{
-    Subject.deleteOne({_id:req.params.id})
-    .then(result=>{
-        console.log(result);
-        res.status(200).json({message:"Subject Deleted!"})
-    })
-    .catch(()=>{
-        console.log("Subject is not deleted")
-    })
-});
-
-app.put("/api/subjects/:id",(req, res, next)=>{
-    const subject = new Subject({
-        _id:req.body._id,
-        subjectAadhar: req.body.subjectAadhar,
-        subjectName: req.body.subjectName
-    })
-    Subject.updateOne({_id:req.params.id}, subject)
-        .then(result=>{
-            console.log(result)
-            res.status(200).json({message:"Subject updated successfully!"});
-        })
-        .catch(()=>{
-            console.log("Subject not updated")
-        })
-});
-
-app.get('/api/subjects/:id',(req, res, next)=>{
-    Subject.findById(req.params.id)
-        .then(subject=>{
-            if(subject){
-                res.status(200).json(subject)
-            }else{
-                res.status(404).json({message:"Subject not found"});
-            }
-        })
-        .catch(()=>{
-            console.log("Found error in getting a subject by ID")
-        })
-});
 
 app.use((req, res, next)=>{
     res.send('Hello From Express')
---
+++ backend/routes/subjects.js
@@ -0,0 +1,82 @@
+const express =  require('express');
+
+const router=express.Router();
+
+const Subject = require('../models/subject');
+
+router.post('',(req, res, next) =>{
+    // const subject=req.body;
+    console.log(req.body);
+    const subject=new Subject({
+        subjectAadhar: req.body.subjectAadhar,
+        subjectName: req.body.subjectName
+    });
+    subject.save().then(createdSubject=>{
+        console.log("subject added success")
+        console.log(createdSubject._id)
+        res.status(201).json({
+            message:"Subject added successfully!",
+            subject: {
+                ...createdSubject,
+                id:createdSubject._id
+            }
+        });
+    })
+    .catch(()=>{
+        console.log("Subject NOT saved")
+    });
+})
+
+router.get('',(req, res, next)=>{
+    Subject.find()
+        .then(documents=>{
+            res.status(200).json({
+                message:"Subject fetched successfully", 
+                subjects:documents
+            });
+        })
+        .catch(()=>{console.log("Unable to get documents")});
+});
+
+router.delete('/:id',(req, res, next)=>{
+    Subject.deleteOne({_id:req.params.id})
+    .then(result=>{
+        console.log(result);
+        res.status(200).json({message:"Subject Deleted!"})
+    })
+    .catch(()=>{
+        console.log("Subject is not deleted")
+    })
+});
+
+router.put("/:id",(req, res, next)=>{
+    const subject = new Subject({
+        _id:req.body._id,
+        subjectAadhar: req.body.subjectAadhar,
+        subjectName: req.body.subjectName
+    })
+    Subject.updateOne({_id:req.params.id}, subject)
+        .then(result=>{
+            console.log(result)
+            res.status(200).json({message:"Subject updated successfully!"});
+        })
+        .catch(()=>{
+            console.log("Subject not updated")
+        })
+});
+
+router.get('/:id',(req, res, next)=>{
+    Subject.findById(req.params.id)
+        .then(subject=>{
+            if(subject){
+                res.status(200).json(subject)
+            }else{
+                res.status(404).json({message:"Subject not found"});
+            }
+        })
+        .catch(()=>{
+            console.log("Found error in getting a subject by ID")
+        })
+});
+
+module.exports=router;
